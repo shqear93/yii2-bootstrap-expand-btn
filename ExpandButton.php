37ade51ce23733191c40e70acfa95464406aaa70
Khaled AbuShqear <qmax93@gmail.com>
2016-04-27 17:55:18 +0300

    inititate repo

---
+++ ExpandButton.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * @link    http://foundationize.com
+ * @package foundationize/yii2-foundation
+ * @version 1.0.0
+ */
+
+namespace shqear\bootstrap;
+
+use yii\base\Widget;
+use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
+
+/**
+ * Description of ExpandButton
+ */
+class ExpandButton extends Widget
+{
+    public $label = 'Button';
+
+    /**
+     * @var array the HTML attributes of the button.
+     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
+     */
+    public $options = ['class' => 'button'];
+
+    /**
+     * The button url, used only for split button
+     * @var string Button url
+     */
+    public $url = '#';
+
+    /**
+     * @var string the tag to use to render the button
+     */
+    public $tagName = 'button';
+
+    /**
+     * @var boolean whether the label should be HTML-encoded.
+     */
+    public $encodeLabel = true;
+
+    /**
+     * Renders the widget.
+     */
+    public function run()
+    {
+        /*$dropdown = Dropdown::begin($this->getDropdownConfig());
+        echo "\n" . $this->renderButton($dropdown);
+        Dropdown::end();
+
+        $this->registerPlugin('button');*/
+    }
+
+    /**
+     * Generates the button dropdown.
+     * @return string the rendering result.
+     */
+    protected function renderButton($dropdown)
+    {
+        /*$dropdownId = $dropdown->getId();
+
+        $label = $this->label;
+        if ($this->encodeLabel) {
+            $label = Html::encode($label);
+        }
+        if ($this->split) {
+            $this->tagName = 'a';
+            Html::addCssClass($this->options, 'button');
+            Html::addCssClass($this->options, 'split');
+            $options = $this->options;
+            $label .= Html::tag('span', '', ['data-dropdown' => $dropdownId]);
+        } else {
+            Html::addCssClass($this->options, 'dropdown');
+            $options = $this->options;
+
+            $options['data-dropdown'] = $dropdownId;
+        }
+
+        return Button::widget([
+            'tagName' => $this->tagName,
+            'label' => $label,
+            'options' => $options,
+            'url' => $this->url,
+            'encodeLabel' => false,
+        ]) . "<br />\n";*/
+    }
+
+    /**
+     * Get config for [[Dropdown]] widget
+     * @return array config options
+     */
+    protected function getDropdownConfig()
+    {
+        /*$config = $this->dropdown;
+        $config['id'] = ArrayHelper::getValue($config, 'id', null);
+        $config['clientOptions'] = false;
+        $config['view'] = $this->getView();
+
+        return $config;*/
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,45 @@
+Yii2 Extension:
+
+Yii2 Bootstrap Expand Btn
+=============
+Provide expanding buttons for Bootstrap 3.
+
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist shqear/bootstrap-expand-btn "*"
+```
+
+or add
+
+```
+"shqear/bootstrap-expand-btn": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+![Bootstrap Expand Btn](expand-btn.gif)
+
+Usage
+-----
+coming soon...
+
+Examples
+--------
+coming soon...
+
+Legals
+------
+based on : http://smarchal.com/bootstrap-expand-btn
+
+- Author : [Khaled AbuShqear] (Qmax93@Gmail.com)
+- Licence : [MIT](http://opensource.org/licenses/MIT) 
+- Contact : [@zessx](https://fb.me/shqear)
+- Link  : [http://smarchal.com/bootstrap-expand-btn]()
